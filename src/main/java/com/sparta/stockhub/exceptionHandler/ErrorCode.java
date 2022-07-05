77704c56a43917deea5ecb823dcd7c66e4162770
joo-hui <02hjooo@gmail.com>
2022-07-05 22:51:40 +0900

    adit : ErrorCode class 오타 수정

--- src/main/java/com/sparta/stockhub/exceptionHandler/ErrorCode.java
+++ src/main/java/com/sparta/stockhub/exceptionHandler/ErrorCode.java
@@ -9,10 +9,10 @@ public enum ErrorCode {
     // 400 BAD_REQUEST
     BAD_REQUEST_NOTWRITE(HttpStatus.BAD_REQUEST, "400", "제목 작성이 필요합니다."),
     BAD_REQUEST_STOCKNAME(HttpStatus.BAD_REQUEST, "400", "종목 선택이 필요합니다."),
-    BAD_REQUEST_TITLELENGTH(HttpStatus.BAD_REQUEST, "400", "제목은 40자 이내로 작성해주세요."),
-    BAD_REQUEST_POINTLENGTH(HttpStatus.BAD_REQUEST, "400", "투자포인트는 40자 이내로 작성해주세요."),
-    BAD_REQUEST_CONTENTLENGTH(HttpStatus.BAD_REQUEST, "400", "세부 내용은 800자 이내로 작성해주세요."),
-    BAD_REQUEST_COMMENTLENGTH(HttpStatus.BAD_REQUEST, "400", "댓글 내용은 300자 이내로 작성해주세요."),
+    BAD_REQUEST_TITLELENGTH(HttpStatus.BAD_REQUEST, "400", "제목은 40자 이내로 작성해 주세요."),
+    BAD_REQUEST_POINTLENGTH(HttpStatus.BAD_REQUEST, "400", "투자 포인트는 40자 이내로 작성해 주세요."),
+    BAD_REQUEST_CONTENTLENGTH(HttpStatus.BAD_REQUEST, "400", "세부 내용은 800자 이내로 작성해 주세요."),
+    BAD_REQUEST_COMMENTLENGTH(HttpStatus.BAD_REQUEST, "400", "댓글 내용은 300자 이내로 작성해 주세요."),
 
 
     // 401 Unauthorized
@@ -22,7 +22,7 @@ public enum ErrorCode {
     // 403 FORBIDDEN
     FORBIDDEN_MYARTICLEVOTE(HttpStatus.FORBIDDEN, "403", "본인 게시글에 투표할 수 없습니다."),
     FORBIDDEN_OLDVOTEUP(HttpStatus.FORBIDDEN, "403", "이미 찬성 투표를 하였습니다."),
-    FORBIDDEN_OLDVOTEDOWN(HttpStatus.FORBIDDEN, "403", "이미 반대 푸툐를 하였습니다."),
+    FORBIDDEN_OLDVOTEDOWN(HttpStatus.FORBIDDEN, "403", "이미 반대 투표를 하였습니다."),
 
 
     // 404 NOT_FOUND
