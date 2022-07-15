d8a8130199e8b7c39d9689b59a656553b8c3c50a
gaius365 <gaius365@gmail.com>
2022-07-15 17:32:52 +0900

    add: 스케쥴러 추가

--- src/main/java/com/sparta/stockhub/StockHubApplication.java
+++ src/main/java/com/sparta/stockhub/StockHubApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 @EnableJpaAuditing
 @EnableMongoRepositories(basePackageClasses = StockRepository.class)
 public class StockHubApplication {
